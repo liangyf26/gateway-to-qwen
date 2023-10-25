ec2f2c399be7e7ebb1a4edc997e4eee3c5d1afc3
peachimmortal6 <76577743@qq.com>
2023-10-25 09:10:30 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,26 +1,23 @@
-
 const TELEGRAPH_URL = 'https://dashscope.aliyuncs.com/api/v1/services/aigc/text-generation/generation';
-
 
 export default {
   async fetch(request, env) {
       const NewResponse = await handleRequest(request)
       return NewResponse
   },
-
 };
 
 async function handleRequest(request) {
-  const url = new URL(request.url);
   const headers_Origin = request.headers.get("Access-Control-Allow-Origin") || "*"
-  // url.host = TELEGRAPH_URL.replace(/^https?:\/\//, '');
-  url.host = TELEGRAPH_URL;
+  
+  // 我们直接使用TELEGRAPH_URL， 不再需要 URL(request.url)
+  const newURL = TELEGRAPH_URL;
 
   // 创建一个新的 Headers 对象复制原来的 headers，然后添加你的 Authorization
   const newHeaders = new Headers(request.headers);
   newHeaders.set('Authorization', 'Bearer sk-ad7cf22cf6b146099f59735c85ec7d33'); 
 
-  const modifiedRequest = new Request(url.toString(), {
+  const modifiedRequest = new Request(newURL, {
     headers: newHeaders,
     method: request.method,
     body: request.body,

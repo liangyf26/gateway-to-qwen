b2fa4214c7660ea058210e8a3cff8d8f2946d545
peachimmortal6 <76577743@qq.com>
2023-10-26 18:36:03 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,10 +1,32 @@
 const TELEGRAPH_URL = 'https://dashscope.aliyuncs.com/api/v1/services/aigc/text-generation/generation';
+let LOGFLARE_API_KEY = 'OX9_n1kPX8_p';
+let LOGFLARE_SOURCE_ID = 'fd15aaf1-8dec-4c6a-bd44-56e57b0c93e2';
+let headersStr = '';
 
 export default {
   async fetch(request, env) {
       const NewResponse = await handleRequest(request)
       return NewResponse
   },
+};
+
+async function sendLogToLogflare(logData) {
+    const url = new URL('https://api.logflare.app/logs');
+    url.searchParams.append('source', LOGFLARE_SOURCE_ID);
+
+    let init = {
+      method: 'POST',
+      headers: {
+        'X-API-KEY': LOGFLARE_API_KEY,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        metadata: {'gateway': 'cloudflare', 'app': 'openai'},
+        message: logData,
+      }),
+    };
+  
+    await fetch(url, init);
 };
 
 async function handleRequest(request) {

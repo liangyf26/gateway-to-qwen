5af8e446e74129b09ac70b0371412995136cf72c
peachimmortal6 <76577743@qq.com>
2023-10-27 10:55:19 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,4 +1,6 @@
 const TELEGRAPH_URL = 'https://dashscope.aliyuncs.com/api/v1/services/aigc/text-generation/generation';
+let LOGFLARE_API_KEY = 'OX9_n1kPX8_p';
+let LOGFLARE_SOURCE_ID = 'fd15aaf1-8dec-4c6a-bd44-56e57b0c93e2';
 
 export default {
   async fetch(request, env) {
@@ -6,6 +8,42 @@ export default {
       return NewResponse
   },
 };
+
+
+async function sendLogToLogflare(logData) {
+  const url = new URL('https://api.logflare.app/logs');
+  url.searchParams.append('source', LOGFLARE_SOURCE_ID);
+
+  let init = {
+    method: 'POST',
+    headers: {
+      'X-API-KEY': LOGFLARE_API_KEY,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      metadata: {'gateway': 'cloudflare', 'app': 'qwen'},
+      event_message: logData
+    }),
+  };
+
+  await fetch(url, init);
+};
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+  event.waitUntil(handleErrors(event))
+})
+
+
+async function handleErrors(event) {
+  try {
+    await event.passThroughOnException()
+  } catch (error) {
+    // 在这里你可以发送错误信息到一些日志服务
+    // 或者发送到你的邮箱，或者使用你喜欢的错误追踪工具
+    sendLogToLogflare('Caught an error:', error)
+  }
+}
 
 async function handleRequest(request) {
   const headers_Origin = request.headers.get("Access-Control-Allow-Origin") || "*"
